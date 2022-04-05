1089084b8cae34c19f07f382f6d60381e28164e7
Gary Yau <gary.yau@hotmail.com>
2022-04-05 23:52:19 +0900

    changed numbering

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <ul>
     <li>
-      <p>No.1</p>
+      <p>Number 1</p>
     </li>
     <li>
-      <p>No.2</p>
+      <p>Number 2</p>
     </li>
   </ul>,
   document.getElementById("root")
